0a2b764b4f533dfe68b8f254b0ceb3af89986daf
Abhigna Nannapaneni <158666377+Abhigna-Nannapaneni@users.noreply.github.com>
2025-11-28 00:11:57 +0530

    Update CorsConfig.java

--- src/main/java/b10/com/example/b10/Config/CorsConfig.java
+++ src/main/java/b10/com/example/b10/Config/CorsConfig.java
@@ -12,12 +12,15 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:30082")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins("http://localhost:30082")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
+}
+
             }
         };
     }
